dbef354c9965cbbfb51467a60f917bd120b462c5
MdAsraful56 <work.mdasraful56@gmail.com>
2025-01-12 22:24:48 +0600

    Header complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './component/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <div className="">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center p-4 border-b-2  max-w-7xl mx-auto'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            
+        </header>
+    );
+};
+
+export default Header;
